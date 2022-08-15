e42b0ba2ee5b4df27114f3a16ec17159922a6295
Plamen Balinov <pbalinov@icloud.com>
2022-08-15 20:44:34 +0300

    Initial Commit

---
+++ QuitYourApps/QuitYourAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitYourAppsApp.swift
+//  QuitYourApps
+//
+//  Created by Plamen Balinov on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct QuitYourAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
